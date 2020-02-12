281d32356e1437e3f96d56e798b95930a19e74e1
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-02-11 19:32:59 -0800

    new summaries page with anchors and sidebar menu

--- app/controllers/scratch_controller.rb
+++ app/controllers/scratch_controller.rb
@@ -37,6 +37,27 @@ class ScratchController < ApplicationController
     data_table.add_rows(Measure.measures_written_per_year_count.map{|year,count| [year.to_s,count] })
     options = { legend: {position: "none"}, height: "600", bar: {width: "16"} }
     @chart = GoogleVisualr::Interactive::ColumnChart.new(data_table, options)
+  end
+
+  def summaries_old
+    @models = []
+    Dir.foreach("app/models") { |model_path|
+      puts model_path
+      if model_path.include?(".rb")
+        next if model_path == "application_record.rb"
+        @models << model_path.gsub(".rb","").gsub("_"," ").split(" ").map{|string| string.capitalize }.join("").pluralize
+      end
+    }
+
+    data_table = GoogleVisualr::DataTable.new
+    # Add Column Headers
+    data_table.new_column('string', 'Year' )
+    data_table.new_column('number', 'Measures Written')
+
+    # Add Rows and Values
+    data_table.add_rows(Measure.measures_written_per_year_count.map{|year,count| [year.to_s,count] })
+    options = { legend: {position: "none"}, height: "600", bar: {width: "16"} }
+    @chart = GoogleVisualr::Interactive::ColumnChart.new(data_table, options)
     
   end
 end
